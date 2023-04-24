bec29974f9ddd1b49cb7d96fc6872bd6c4b9029f
jerita <jchibhabha1@gmail.com>
2023-04-24 10:51:19 -0700

    printf collab

--- main.h
+++ main.h
@@ -7,6 +7,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int switch_num(va_list, char, int);
